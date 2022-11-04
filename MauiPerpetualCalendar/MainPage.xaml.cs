9999ab0237addfd80fa64b3fda30881f5b4c084e
David Morrow <quickbasic44@outlook.com>
2022-11-04 14:11:38 -0400

    Add project files.

---
+++ MauiPerpetualCalendar/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace MauiPerpetualCalendar;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        PerpetualCalendar calendar = new PerpetualCalendar();
+        int year = int.Parse(entryNumber.Text);
+
+        Debug.WriteLine(calendar.YearList.Count);
+        Debug.WriteLine(calendar.YearList.Values.Count);
+
+        int CalNumber = 0;
+
+        foreach (KeyValuePair<int, int[]> item in calendar.YearList)
+        {
+            CalNumber++;
+            foreach (int val in item.Value)
+            {
+                if (val == year)
+                {
+                    Debug.WriteLine($"Calendar number is {CalNumber}");
+                    break;
+                }
+            }
+        }
+
+    }
+}
+
---
+++ MauiPerpetualCalendar/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiPerpetualCalendar;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
